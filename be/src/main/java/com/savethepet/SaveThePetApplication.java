e9f61d2bab9f77519a654f807f4f2e1d153b8cc3
BrineFast <brineone@gmail.com>
2020-03-18 22:11:27 +0400

    Backend package structure

---
+++ be/src/main/java/com/savethepet/SaveThePetApplication.java
@@ -0,0 +1,13 @@
+package com.savethepet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveThePetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveThePetApplication.class, args);
+	}
+
+}
