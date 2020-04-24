ba9d427c1ccdabcc1aebcb41c082edf27e47696e
Chekh <chekhprogrammer@gmeil.com>
2020-04-24 17:48:39 +0400

    TASK-3: Fix login redirect and oauth workin correctly

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { map, catchError } from 'rxjs/operators';
 import { HttpService } from '../http.service'
-import { HttpClient, HttpEventType } from '@angular/common/http';
+import { HttpClient, HttpEventType, HttpEvent } from '@angular/common/http';
 import { User } from '../../user'
 import { NgForm } from '@angular/forms'
 import { Router } from '@angular/router';
@@ -34,7 +34,7 @@ export class LoginComponent /*implements OnInit*/ {
 
   onSubmit(e) {
     this.http.loginin(this.profileForm.value.login, this.profileForm.value.password).subscribe(
-      response =>
+      /*response =>
       {
         console.log(response);
         console.log(response.status);
@@ -44,6 +44,17 @@ export class LoginComponent /*implements OnInit*/ {
       err =>
       {
         console.log(err.status);
+      }*/
+      (progressEvent: HttpEvent<any>) => {
+        switch (progressEvent.type) {
+          case HttpEventType.Response:
+            console.log('ОК');
+            this.router.navigate(["/home"]);
+            break;
+          case HttpEventType.Sent:
+            console.log('Sent, but not OK');
+            break;
+        }
       }
     );
   }
